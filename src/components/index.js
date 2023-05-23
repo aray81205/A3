539ae327867a537eef7d5833eb4d5bf199ba2c19
ARAY81205 <ARAY81205@gmail.com>
2023-05-23 21:57:42 +0800

    Fix Name2

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Header from'./header/header'
-import Main from './main/main'
-import Footer from './footer/footer'
+import Header from'./Header/header'
+import Main from './main/Main'
+import Footer from './Footer/Footer'
 export{ Header, Main, Footer}
